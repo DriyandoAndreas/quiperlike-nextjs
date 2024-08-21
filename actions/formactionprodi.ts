cf46c3c86fb82f264fc536e32e0860bc00677360
DriyandoAndreas <ragecoderid@gmail.com>
2024-08-22 00:42:15 +0700

    update edit form fix the id

--- actions/formactionprodi.ts
+++ actions/formactionprodi.ts
@@ -70,21 +70,6 @@ export async function Create(formData: FormData) {
 export async function Edit(formData: FormData) {
   try {
     const id = Number(formData.get("id"));
-    console.log("ID to update:", id);
-
-    // Cek apakah record dengan id tersebut ada di database
-    const existingProdi = await prisma.prodi.findFirst({
-      where: { prodi_id: id },
-    });
-
-    if (!existingProdi) {
-      console.error("Record not found");
-      return {
-        status: 404,
-        message: "Data tidak ditemukan",
-      };
-    }
-
     const namaprodi = formData.get("nama_prodi") as string;
     const deskripsiprodi = formData.get("deskripsi_prodi") as string;
     const prospekkerja = formData.get("prospek_kerja_prodi") as string;
@@ -95,12 +80,11 @@ export async function Edit(formData: FormData) {
     const duniaperkuliahan = formData.get("dunia_perkuliahan") as string;
     const url_banner = formData.get("url_banner") as string;
     const url_kampus_terkait = formData.get("url_image_kampus_terkait") as string;
-
     await prisma.prodi.update({
       where: {
-        prodi_id: id,
+        prodi_id : id,
       },
-      data: {
+     data: {
         nama_prodi: namaprodi,
         deskripsi_prodi: deskripsiprodi,
         prospek_kerja_prodi: prospekkerja,
@@ -113,11 +97,10 @@ export async function Edit(formData: FormData) {
         url_image_kampus_terkait: url_kampus_terkait,
       },
     });
-
-    revalidatePath("/dashboard/prodi");
+    revalidatePath("/dashboard/prodi")
     return {
       status: 200,
-      message: "Data berhasil diedit",
+      message: "berhasil ditambahkan",
     };
   } catch (error) {
     console.error("Error:", error);
@@ -127,7 +110,6 @@ export async function Edit(formData: FormData) {
     };
   }
 }
-
 //delete
 export async function Delete(id:number) {
   try {
